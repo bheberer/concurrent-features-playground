12a67128333fe47ad095b70b2b947c41a518ba61
bheberer <billyheb@gmail.com>
2022-03-13 17:49:47 -0700

    app w/o concurrent features

--- src/App.js
+++ src/App.js
@@ -1,23 +1,122 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {
+  VictoryChart,
+  VictoryScatter,
+  VictoryContainer,
+  VictoryTheme,
+  VictoryBar,
+  VictoryStack,
+  VictoryArea,
+} from 'victory';
+
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const data = value.split('').reduce((acc) => {
+    const points = [];
+
+    for (let i = 0; i < 25; i++) {
+      points.push({
+        x: Math.random() * 20,
+        y: Math.random() * 200,
+        fill: i % 3 === 0 ? '#E6497A' : i % 2 === 0 ? '#3EB0E6' : '#E6D765',
+      });
+    }
+
+    return [...acc, ...points];
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <section>
+        <label for="user-input" style={{ color: 'white', marginLeft: 48, display: 'block' }}>
+          <h1>Very fun concurrency demo</h1>
+          <input
+            id="user-input"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            style={{ width: 400, height: 40, fontSize: 16 }}
+            placeholder="longer input -> more dom nodes -> slow :("
+          />
+        </label>
+
+        <div style={{ display: 'flex' }}>
+          <VictoryChart
+            theme={VictoryTheme.material}
+            height={400}
+            width={400}
+            domain={{ x: [0, 20], y: [0, 200] }}
+            containerComponent={<VictoryContainer responsive={false} />}
+          >
+            <VictoryScatter
+              size={5}
+              data={data}
+              style={{
+                data: {
+                  fill: ({ datum }) => {
+                    return datum.fill;
+                  },
+                },
+              }}
+            />
+          </VictoryChart>
+          <VictoryChart
+            theme={VictoryTheme.material}
+            height={400}
+            width={400}
+            domain={{ x: [0, 20], y: [0, 200] }}
+            containerComponent={<VictoryContainer responsive={false} />}
+          >
+            <VictoryBar
+              size={5}
+              data={data}
+              style={{
+                data: {
+                  fill: ({ datum }) => {
+                    return datum.fill;
+                  },
+                },
+              }}
+            />
+          </VictoryChart>
+        </div>
+        <VictoryChart
+          theme={VictoryTheme.material}
+          height={400}
+          width={800}
+          domain={{ x: [0, 20], y: [0, 200] }}
+          containerComponent={<VictoryContainer responsive={false} />}
         >
-          Learn React
-        </a>
-      </header>
+          <VictoryStack>
+            <VictoryArea
+              data={data}
+              style={{
+                data: {
+                  fill: '#E6497A',
+                },
+              }}
+            />
+            <VictoryArea
+              data={data}
+              style={{
+                data: {
+                  fill: '#3EB0E6',
+                },
+              }}
+            />
+            <VictoryArea
+              data={data}
+              style={{
+                data: {
+                  fill: '#E6D765',
+                },
+              }}
+            />
+          </VictoryStack>
+        </VictoryChart>
+      </section>
     </div>
   );
 }
